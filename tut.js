89f8e362358a7960d0968628a61fe3df64787ebc
SagarKumarMathuri <98569731+SagarKumarMathuri@users.noreply.github.com>
2023-05-10 22:14:02 +0530

    Add files via upload

---
+++ tut.js
@@ -0,0 +1,13 @@
+console.time('your code took');
+console.log('Hello console');
+
+console.log(22+44);
+console.log(true);
+console.log([33,33,22,44,44])
+console.log({harry:'this',mark:33})
+console.table({vikaral:'this',mark:33})
+console.warn('this is warning')
+//console.clear()
+console.timeEnd('your code took');
+//console.assert(333<331,'age>331 is not possible')
+console.error('this is error')
